fc75ef5a178736aee280759fb4806b694afff7c2
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2023-01-29 22:20:05 +0100

    @SOAPBinding to config <binding>, what is more decide <types> from Remote Procedure Controll or xsd document

--- src/main/java/shop/ShopInfo.java
+++ src/main/java/shop/ShopInfo.java
@@ -6,12 +6,15 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 import javax.xml.ws.BindingType;
 
 @WebService
-//@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
-@BindingType(javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
+//@SOAPBinding(style = SOAPBinding.Style.RPC)
+// @BindingType(javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
 public class ShopInfo {
+   /*
     @WebMethod
     @WebResult(partName = "lookupOutput")
     public String getShopInfo(@WebParam(partName = "lookupInput") String property) throws Exception {
@@ -27,6 +30,17 @@ public class ShopInfo {
             //throw new InvalidInputException("Invalid input", " not a valid input");
             throw new Exception("Invalid input");
         }
+        return response;
+    }
+
+    */
+    @WebMethod
+    public String getShopInfoSample1(String property) {
+        String response = "Invalid property";
+        if("shopName".equals(property))
+            response = "shopName";
+        else if ("since".equals(property))
+            response = "since";
         return response;
     }
 }
